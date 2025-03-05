1947d3fe3900dde7eab7bf5304949a82eda8345c
Anis Chebbi <97995627+chebbianis@users.noreply.github.com>
2025-03-06 00:50:21 +0100

    page contact et page facture

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                // Vous pouvez personnaliser vos couleurs ici
+            },
+            fontFamily: {
+                // Vous pouvez définir vos polices ici
+            },
+        },
+    },
+    plugins: [],
+} 
