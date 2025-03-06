b55823a1db51e8a69faf6d596000d3fc2a1d8617
hamoudachekir <hamoudachkir@yahoo.fr>
2025-03-06 02:31:12 +0100

    Fusion de la branche hamouda dans main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
     content: ["./**/*.{html,js}"],
     theme: {
         extend: {
@@ -13,3 +14,25 @@ module.exports = {
     },
     plugins: [],
 } 
+=======
+  content: ["./*.html", "./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E63946",  // Rouge de ta bannière
+        secondary: "#457B9D", // Bleu optionnel si besoin
+        background: "#F7F9FC", // Couleur de fond de la maquette
+        textDark: "#333333", // Texte sombre si besoin
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"], // Police moderne, à adapter si ta maquette a une autre typo
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+    },
+  },
+  plugins: [],
+}
+>>>>>>> fc8309c (Ajout du projet LunchBox Pro)
